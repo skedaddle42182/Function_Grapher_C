b6cec17b615f175db11c8385b87caa73f3536eb1
skedaddle42182 <killuazoldycklightning6@proton.me>
2026-06-02 23:47:45 -0400

    Delete engine.cpp

--- engine.cpp
+++
@@ -1,8 +0,0 @@
-#include "Engine.hpp"
-
-Engine::Engine(WindowProperties& window_properties, GridProperties& grid_properties)
-{
-	window.create(window_properties);
-	renderer.create(window);
-	grid.create(window, grid_properties.rows, grid_properties.columns, grid_properties.cell_scale_y, grid_properties.cell_scale_x);
-}
